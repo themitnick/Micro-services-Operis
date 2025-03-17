871acef3aa6fe345b9c7d3bf643b5150de30c233
Nkonan <yaotyrone@gmail.com>
2025-03-17 12:33:41 +0100

    Implementing of Server config

---
+++ Operis-config/src/main/java/ci/operis/OperisConfigApplication.java
@@ -0,0 +1,13 @@
+package ci.operis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class OperisConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OperisConfigApplication.class, args);
+    }
+}
