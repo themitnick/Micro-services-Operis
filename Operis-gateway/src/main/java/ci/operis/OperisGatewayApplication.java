ba4811f27d3efaa30c62c3cf97f8135524d5f44f
Nkonan <yaotyrone@gmail.com>
2025-03-17 11:45:50 +0100

    Implementing of Gateway

---
+++ Operis-gateway/src/main/java/ci/operis/OperisGatewayApplication.java
@@ -0,0 +1,11 @@
+package ci.operis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperisGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OperisGatewayApplication.class, args);
+    }
+}
